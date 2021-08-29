563c358c7b4a4670d170e70d580aa882ab856aeb
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2021-08-29 16:42:36 +0000

    change some stuff

--- public/js/game/chat.js
+++ public/js/game/chat.js
@@ -6,3 +6,21 @@ window.onblur = function(){
 window.onfocus = function(){
   focus = true;
 }
+
+document.querySelector("input").addEventListener("keydown", e => {
+  
+});
+
+function encodeHTML(text){
+  var div = document.createElement("div");
+  div.innerText = text;
+  return div.innerHTML;
+}
+
+function validMessage(message){
+  if(!message) return false;
+  for(var i of message){
+    if(i != " ") return true;
+  }
+  return false;
+}
