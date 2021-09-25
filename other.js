6d92015e7f79d4cc1ab3a207950987944313a24e
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2021-09-25 01:42:39 +0000

    creat other.js

---
+++ other.js
@@ -0,0 +1,12 @@
+const Database = require("@replit/database");
+const db = new Database();
+
+async function del(){
+  let keys = await db.list();
+  for(let i of keys){
+    
+    }
+  }
+}
+
+del()
